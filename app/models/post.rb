6580a70881464d5b4db70b9cd28a025ed74dfbfb
Juan Rosario <jrosario19@hotmail.com>
2022-08-10 11:11:10 -0400

    Create model for user, post, comment and like

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+  
+    after_save :increment_user_posts_count
+  
+    def recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
+  
+    private
+  
+    def increment_user_posts_count
+      author.increment!(:posts_counter)
+    end
+  end
