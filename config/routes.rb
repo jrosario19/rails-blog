9cde30d27a3ab3c0ffda2a61c5865bd41afd5888
Juan Rosario <jrosario19@hotmail.com>
2022-08-18 08:34:56 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
+    post "/posts/:id/like" => "posts#like", as: :like
+    post "/posts/:id/comment" => "posts#comment", as: :comments
   end
 end
