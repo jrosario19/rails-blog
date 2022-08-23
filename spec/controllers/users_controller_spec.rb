820f7bd02fcc9820eec841be973d0c5cbf7fdf46
Juan Rosario <jrosario19@hotmail.com>
2022-08-23 09:49:40 -0400

    Add integration test for post show

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /users' do
+    User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
     it 'response for index action status is correct' do
       get users_path
       expect(response).to have_http_status(200)
@@ -14,22 +15,23 @@ RSpec.describe 'Users', type: :request do
 
     it 'the response body of index includes correct placeholder text' do
       get users_path
-      expect(response.body).to include('Number of posts')
+      expect(response.body).to include(User.last.name)
     end
   end
 
   describe 'GET /users/:id' do
+    User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
     it 'response for show action status is correct' do
-      get user_path(1)
+      get user_path(User.last)
       expect(response).to have_http_status(200)
     end
     it 'renders the show template' do
-      get user_path(1)
+      get user_path(User.last)
       expect(response).to render_template('show')
     end
     it 'the response body of show includes correct placeholder text' do
-      get user_path(1)
-      expect(response.body).to include('Number of posts')
+      get user_path(User.last)
+      expect(response.body).to include(User.last.bio)
     end
   end
 end
---
+++ spec/integration/post_show_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe 'Post Show', type: :system do
+  Post.destroy_all
+  User.destroy_all
+  it 'can see the post\'s title.' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('post1')
+  end
+  it 'can see who wrote the post' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('henry')
+  end
+  it 'can see how many comments it has' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    Comment.create(text: 'comment1', author:user, post:post)
+    Comment.create(text: 'comment2', author:user, post:post)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('1')
+    expect(page).to have_content('2')
+  end
+  it 'can see how many likes it has' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    Like.create(author:user, post:post)
+    Like.create(author:user, post:post)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('1')
+    expect(page).to have_content('2')
+  end
+  it 'can see the post body' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('text1')
+  end
+  it 'can see the username of each commentor' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    Comment.create(text: 'comment1', author:user, post:post)
+    Comment.create(text: 'comment2', author:user, post:post)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('henry')
+  end
+  it 'can see the comment each commentor left' do
+    user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
+    post = Post.create(title: 'post1', text: 'text1', author:user)
+    Comment.create(text: 'comment1', author:user, post:post)
+    Comment.create(text: 'comment2', author:user, post:post)
+    visit "/users/#{user.id}/posts/#{post.id}"
+    expect(page).to have_content('comment1')
+    expect(page).to have_content('comment2')
+  end
+end
