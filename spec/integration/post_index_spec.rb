2825db01429105b4109787edd3895118c8db1a2d
Juan Rosario <jrosario19@hotmail.com>
2022-08-23 11:11:54 -0400

    Solve linter errors

--- spec/integration/post_index_spec.rb
+++ spec/integration/post_index_spec.rb
@@ -15,8 +15,8 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see the number of posts' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
     visit '/users'
     expect(page).to have_content('1')
     expect(page).to have_content('2')
@@ -24,8 +24,8 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see the posts title' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
     visit "/users/#{user.id}/posts"
     expect(page).to have_content('post1')
     expect(page).to have_content('post2')
@@ -33,10 +33,10 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see the first comments on a posts' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    post1 = Post.create(title: 'post1', text: 'text1', author:user)
-    post2 = Post.create(title: 'post2', text: 'text2', author:user)
-    Comment.create(text: 'comment1', author:user, post: post1)
-    Comment.create(text: 'comment2', author:user, post: post2)
+    post1 = Post.create(title: 'post1', text: 'text1', author: user)
+    post2 = Post.create(title: 'post2', text: 'text2', author: user)
+    Comment.create(text: 'comment1', author: user, post: post1)
+    Comment.create(text: 'comment2', author: user, post: post2)
     visit "/users/#{user.id}/posts"
     expect(page).to have_content('comment1')
     expect(page).to have_content('comment2')
@@ -44,10 +44,10 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see the number of comments on a post' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Comment.create(text: 'comment1', author:user, post: Post.first)
-    Comment.create(text: 'comment2', author:user, post: Post.last)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Comment.create(text: 'comment1', author: user, post: Post.first)
+    Comment.create(text: 'comment2', author: user, post: Post.last)
     visit '/users'
     expect(page).to have_content('1')
     expect(page).to have_content('2')
@@ -55,10 +55,10 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see the how many likes a post has' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Like.create(author:user, post: Post.first)
-    Like.create(author:user, post: Post.last)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Like.create(author: user, post: Post.first)
+    Like.create(author: user, post: Post.last)
     visit "/users/#{user.id}/posts"
     expect(page).to have_content('1')
     expect(page).to have_content('2')
@@ -66,16 +66,16 @@ RSpec.describe 'Post index', type: :system do
 
   it 'can see a section for pagination' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Post.create(title: 'post3', text: 'text3', author:user)
-    Post.create(title: 'post4', text: 'text4', author:user)
-    Post.create(title: 'post5', text: 'text5', author:user)
-    Post.create(title: 'post6', text: 'text6', author:user)
-    Post.create(title: 'post7', text: 'text7', author:user)
-    Post.create(title: 'post8', text: 'text8', author:user)
-    Post.create(title: 'post9', text: 'text9', author:user)
-    Post.create(title: 'post10', text: 'text10', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Post.create(title: 'post3', text: 'text3', author: user)
+    Post.create(title: 'post4', text: 'text4', author: user)
+    Post.create(title: 'post5', text: 'text5', author: user)
+    Post.create(title: 'post6', text: 'text6', author: user)
+    Post.create(title: 'post7', text: 'text7', author: user)
+    Post.create(title: 'post8', text: 'text8', author: user)
+    Post.create(title: 'post9', text: 'text9', author: user)
+    Post.create(title: 'post10', text: 'text10', author: user)
 
     visit "/users/#{user.id}/posts"
     expect(page).to have_content('1')
@@ -87,8 +87,8 @@ RSpec.describe 'Post index', type: :system do
 
   it 'redirects to Post show page when clicking on a post' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
     visit "/users/#{user.id}/posts"
     click_on 'post1'
     expect(page).to have_content('post1')
--- spec/integration/user_show_spec.rb
+++ spec/integration/user_show_spec.rb
@@ -15,18 +15,18 @@ RSpec.describe 'User show', type: :system do
   end
   it 'displays the number of posts of the user' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
     visit "/users/#{user.id}"
     expect(page).to have_content('2')
   end
   it 'displays the first 3 posts of the user' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Post.create(title: 'post3', text: 'text3', author:user)
-    Post.create(title: 'post4', text: 'text4', author:user)
-    Post.create(title: 'post5', text: 'text5', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Post.create(title: 'post3', text: 'text3', author: user)
+    Post.create(title: 'post4', text: 'text4', author: user)
+    Post.create(title: 'post5', text: 'text5', author: user)
     visit "/users/#{user.id}"
     expect(page).to have_content('post5')
     expect(page).to have_content('post4')
@@ -34,32 +34,32 @@ RSpec.describe 'User show', type: :system do
   end
   it 'displays a button to see the rest of the posts' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Post.create(title: 'post3', text: 'text3', author:user)
-    Post.create(title: 'post4', text: 'text4', author:user)
-    Post.create(title: 'post5', text: 'text5', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Post.create(title: 'post3', text: 'text3', author: user)
+    Post.create(title: 'post4', text: 'text4', author: user)
+    Post.create(title: 'post5', text: 'text5', author: user)
     visit "/users/#{user.id}"
     expect(page).to have_link('See all posts')
   end
   it 'redirects to the posts show page when clicking a users post' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Post.create(title: 'post3', text: 'text3', author:user)
-    Post.create(title: 'post4', text: 'text4', author:user)
-    p1 = Post.create(title: 'post5', text: 'text5', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Post.create(title: 'post3', text: 'text3', author: user)
+    Post.create(title: 'post4', text: 'text4', author: user)
+    p1 = Post.create(title: 'post5', text: 'text5', author: user)
     visit "/users/#{user.id}"
     click_link 'post5'
-    expect(page).to have_current_path(user_post_path(user,p1))
+    expect(page).to have_current_path(user_post_path(user, p1))
   end
   it 'redirects to the users posts index page when click see all posts' do
     user = User.create(name: 'henry', photo: '', bio: 'teacher from Poland')
-    Post.create(title: 'post1', text: 'text1', author:user)
-    Post.create(title: 'post2', text: 'text2', author:user)
-    Post.create(title: 'post3', text: 'text3', author:user)
-    Post.create(title: 'post4', text: 'text4', author:user)
-    Post.create(title: 'post5', text: 'text5', author:user)
+    Post.create(title: 'post1', text: 'text1', author: user)
+    Post.create(title: 'post2', text: 'text2', author: user)
+    Post.create(title: 'post3', text: 'text3', author: user)
+    Post.create(title: 'post4', text: 'text4', author: user)
+    Post.create(title: 'post5', text: 'text5', author: user)
     visit "/users/#{user.id}"
     click_link 'See all posts'
     expect(page).to have_current_path(user_posts_path(user))
